998d108860f364cfda89e308f85019ed5bdbd539
Felipe Biel <felipe.biel.souza@gmail.com>
2020-12-16 10:55:47 -0300

    new: Forma de mudar a cor dos icones em hover dos contatos (limitação do boxicons)

---
+++ src/components/home/Home.js
@@ -0,0 +1,11 @@
+export default {
+    name: 'Home',
+    data() {
+        return {
+            colorHover: [
+                '#0e2431',
+                '#0e2431'
+            ]
+        }
+    },
+}

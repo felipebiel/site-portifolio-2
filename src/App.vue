1ab146c45f4461fc806a644929006f2a367015bf
Felipe Biel <felipe.biel.souza@gmail.com>
2020-12-16 10:17:07 -0300

    new: Arquivos base da Home

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <Header></Header>
+    <Home></Home>
   </div>
 </template>
 
 <script>
-import Header from './components/header/Header.vue';
+import Header from '@/components/header/Header.vue';
+import Home from '@/components/home/Home.vue';
 
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Home
   }
 }
 </script>

6ebe82c4daaec0bbee1d10c782f6066ddd5a9f65
Felipe Biel <felipe.biel.souza@gmail.com>
2020-12-16 13:55:49 -0300

    new: Instalando a lib de vue scroll reveal

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import 'boxicons';
 
 import './scss/base.scss';
 
+import VueScrollReveal from 'vue-scroll-reveal';
+
+
+Vue.use(VueScrollReveal);
+
+Vue.use(VueScrollReveal, {
+  class: 'v-scroll-reveal',
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
 Vue.config.productionTip = false
 
 new Vue({
